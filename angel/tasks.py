f13c1404debac000a85b96577362483e533cf340
shivnamha09 <korecent@erp-server.aeronsolution.com>
2015-10-31 09:55:57 -0400

    removed

---
+++ angel/tasks.py
@@ -0,0 +1,33 @@
+
+from __future__ import unicode_literals
+import frappe
+from frappe.celery_app import celery_task, task_logger
+
+@celery_task()
+def sync_document(site, newsletter, event):
+	# hack! pass event="bulk_long" to queue in longjob queue
+	try:
+		frappe.connect(site=site)
+		doc = frappe.get_doc("Newsletter", newsletter)
+		doc.send_bulk()
+
+	except:
+		frappe.db.rollback()
+		task_logger.warn(frappe.get_traceback())
+
+		# wasn't able to send emails :(
+		doc.db_set("email_sent", 0)
+		frappe.db.commit()
+
+		raise
+
+	else:
+		frappe.db.commit()
+
+	finally:
+		frappe.destroy()
+
+
+#prepare API call for second server with session
+ # calling this funciton
+# erpnext.tasks.send_newsletter.delay(frappe.local.site, self.name, event="bulk_long")
