c32ca1ed9ae42b4855f503696cfe132d2ffa58cc
shivnamha09 <korecent@erp-server.aeronsolution.com>
2015-11-02 03:57:15 -0500

    sync

--- angel/tasks.py
+++ angel/tasks.py
@@ -29,6 +29,21 @@ def sync_document(site, newsletter, event):
 
 # https://github.com/frappe/frappe-client
 
+'''
+                        $.ajax("/api/resource/Mode of Payment").success(function(data) {
+                                $.each(data.data, function(i, d) { me.modes_of_payment.push(d.name); }); 
+                                callback();
+                        });
+                        $.ajax({
+                                url: "/api/resource/Report/" + encodeURIComponent(doc.name),
+                                type: "POST",
+                                data: {
+                                        run_method: "toggle_disable",
+                                        disable: doc.disabled ? 0 : 1 
+                                }
+                        })
+self.session.get(self.url + "/api/resource/" + doctype, params=params, verify=self.verify)
+'''
 #prepare API call for second server with session
  # calling this funciton
 # erpnext.tasks.send_newsletter.delay(frappe.local.site, self.name, event="bulk_long")
