ec1d37fce1a3fb54281cdb9a4b997f389e129fed
shivnamha09 <korecent@erp-server.aeronsolution.com>
2015-11-19 06:32:39 -0500

    #1 ERP Sync feature

--- angel/tasks.py
+++ angel/tasks.py
@@ -1,6 +1,7 @@
 
 from __future__ import unicode_literals
 import frappe
+
 from frappe.celery_app import celery_task, task_logger
 from frappe.frappeclient import FrappeClient
 from frappe.tasks import run_async_task
@@ -70,61 +71,91 @@ def post_process(self, response):
                         sys.stderr.write('\n' + END_LINE)
                         sys.stdout.close()
 
+from frappe.celery_app import get_queue 
+
+RSYNC_TASK_PREFIX = "rsync@" 
+# New Document type
+#Remote Document Sync
+class RsyncTaskRouter(object):
+        def route_for_task(self, task, args=None, kwargs=None):
+                if hasattr(frappe.local, 'site'):
+                        if kwargs and kwargs.get("event", "").endswith("_rsync"):
+                                return get_queue(frappe.local.site, RSYNC_TASK_PREFIX)
+
+                return None
+
+
 # New Document type
 #Remote Document Sync
 
 @celery_task()
 #@frappe.async.handler
-def sync_doc():
+def sync_doc(site, doc, event, method):
 	# hack! pass event="bulk_long" to queue in longjob queue
-        print frappe.flags
+        #print frappe.flags
         #print doc 
-        print frappe.local
-        print frappe.__dict(args)
-        form_dict = frappe._dict()
-        print cmd 
-        user = (frappe.session and frappe.session.user)
+        #print frappe.local
+        #print frappe.__dict(args)
+        #form_dict = frappe._dict()
+        #print cmd 
+        #user = (frappe.session and frappe.session.user)
+        form_dict = doc.as_dict()
+        user = (frappe.session and frappe.session.user) or doc_dict['modified_by'] or "Administrator" 
         pwd = frappe.db.sql("""select password from __Auth where user=%s;""",(user))
         if not pwd:
                 return False
+        #print form_dict
+        pwd = pwd[0][0]
+        #print pwd
         client = FrappeClient(frappe.conf.sync_server_ip, user, pwd)
         if not client:
                 return
         remote_doc = frappe.new_doc(form_dict['doctype'])
         for key in remote_doc.as_dict().keys():
-                 remote_doc.set(key, doc.get(key))
-        remote_doc.set('name', 'New ' + doc.get('doctype') + ' 1')
-        print remote_doc.as_dict()
-        return
-        if form_dict['cmd_method'] == "after_insert":
+                 if key == "territory_name" or key == "is_group":
+                         remote_doc.set(key, doc.get(key))
+        #remote_doc.set('name', 'New ' + doc.get('doctype') + ' 1')
+        #print remote_doc.as_dict()
+        print method
+        if method == "after_insert":
                 print client.insert(doc)
                 doc =  frappe.new_doc("Remote Document Sync")
                 doc.source_document_name = form_dict['name']
                 doc.target_document_name = ""                
 
-        elif form_dict['cmd_method'] == "on_trash":
+        elif method == "on_trash":
                 client.delete(doc)
-        elif form_dict['cmd_method'] == "on_update":
+        elif method == "on_update":
+                print client.insert(doc)
+                doc =  frappe.new_doc("Remote Document Sync")
+                doc.source_document_name = form_dict['name']
+                doc.target_document_name = ""                
+                return
                 client.update(doc)
-        elif form_dict['cmd_method'] == "on_cancel":
+        elif method == "on_cancel":
                 client.cancel(doc)
-        elif form_dict['cmd_method'] == "on_submit":
+        elif method == "on_submit":
                 client.submit(doc)
-        elif form_dict['cmd_method'] == "after_rename":
+        elif method == "after_rename":
                 client.rename(doc)
 
 def sync_doc_remote(doc, method):
         #print "args " + args 
         #print "kw " + kw
-        form_dict = frappe._dict()
-        doc_dict = doc.as_dict()
+        #form_dict = frappe._dict()
+        #doc_dict = doc.as_dict()
         #print doc_dict['name']
         #print doc_dict['doctype']
        
-        form_dict['cmd_method'] = method 
-        user = (frappe.session and frappe.session.user) or doc_dict['modified_by'] or "Administrator" 
-        result = run_async_task.delay(site=frappe.local.site, user=user, cmd='sync_doc', form_dict=form_dict)
-        #print result.get()
+        #form_dict['cmd_method'] = method 
+        #doc.set('cmd_method1') = method
+        user = (frappe.session and frappe.session.user) or doc.get('modified_by') or "Administrator" 
+        #result = run_async_task.delay(site=frappe.local.site, user=user, cmd='sync_doc', form_dict=form_dict)
+        #result = sync_doc.delay(site=frappe.local.site, event="doc_rsync", form_dict=doc_dict)
+        #print "chetan execut sync_doc"
+        sync_doc(site=frappe.local.site, event="doc_rsync", doc=doc , method = method)
+       
+        #print result
 
 
 # https://github.com/frappe/frappe-client
