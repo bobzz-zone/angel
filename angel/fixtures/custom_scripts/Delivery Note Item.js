a266e4d21b7dbcef580407b1726a5830d76f233f
Chetan sharma <chetan.sh009@gmail.com>
2015-12-12 09:14:58 +0000

    Delivery Note Item.js edited online with Bitbucket

--- angel/fixtures/custom_scripts/Delivery Note Item.js
+++ angel/fixtures/custom_scripts/Delivery Note Item.js
@@ -1,11 +1,13 @@
+
 frappe.ui.form.on("Delivery Note Item", "price_list_rate", function(doc, cdt, cdn){
   var d = locals[cdt][cdn];
+//debugger ;
   var price_list = frappe.model.get_value("Delivery Note Item", cdn, "price_list");
-
   if ( price_list == "" || ! price_list){
      frappe.model.set_value("Delivery Note Item", cdn, "price_list_rate", 0.0);
      return;
   }
+//debugger ;
   var item_code = frappe.model.get_value("Delivery Note Item", cdn, "item_code");
   frappe.model.get_value("Item Price", {
                      "item_code": item_code,
@@ -14,7 +16,9 @@ frappe.ui.form.on("Delivery Note Item", "price_list_rate", function(doc, cdt, cd
                     function(r){
                          frappe.model.set_value("Delivery Note Item", cdn, "price_list_rate", r.price_list_rate);
                     });
-
+   
+ 
+  //debugger;
   //cur_frm.fields_dict.items.grid.grid_rows_by_docname[cdn].refresh_field('price_list_rate');
 });
 
@@ -22,4 +26,4 @@ frappe.ui.form.on("Delivery Note Item", "price_list", function(doc, cdt, cdn){
        var d = locals[cdt][cdn];
        frappe.model.trigger("price_list_rate", 
                       frappe.model.get_value("Delivery Note Item", cdn, "price_list_rate"), d);
- }); 
+ });
