091f37b66af3a41f114629f11796c7a2841c08aa
shivnamha09 <korecent@erp-server.aeronsolution.com>
2015-12-19 06:30:30 -0500

    Angel#12 post_to journal

---
+++ angel/angel/doctype/petty_cash/petty_cash.js
@@ -0,0 +1,37 @@
+
+frappe.ui.form.on("Petty Cash", { refresh: function(frm){
+	var pay_for = frm.doc.pay_for;
+	var amount_tertagih =frm.doc.amount_tertagih;
+	var status = cint(frm.doc.docstatus);
+	if(status == 1){
+		frm.add_custom_button(__("Post To Journal"),function(frm){
+			var JE = frappe.model.get_new_doc("Journal Entry");
+			frappe.model.with_doctype("Journal Entry",function(){
+			for(var i = 0; i < 2 ; i++){
+			var child_doc_type = frappe.model.get_new_doc("Journal Entry Account", JE, "accounts"); 
+				console.log(frm);
+				if(i ==0){
+				$.extend(child_doc_type,{
+				"account": "Petty Cash A/C - BC",
+				"debit_in_account_currency":amount_tertagih
+	});
+}			else {
+			$.extend(child_doc_type,{
+				"account": pay_for,
+				"credit_in_account_currency":amount_tertagih
+});
+			}
+		
+}	
+			frappe.set_route("Form", "Journal Entry", JE.name);
+		
+	})
+
+}
+ ).addClass("btn-primary");
+		
+	}
+
+
+}});
+
