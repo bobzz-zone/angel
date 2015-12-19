dcae34cab03751f769a4d54cd3179815fdf91ae7
shivnamha09 <korecent@erp-server.aeronsolution.com>
2015-12-19 00:57:13 -0500

    removed debug message

--- angel/angel/report/territory_based_undelivered_sales_order/territory_based_undelivered_sales_order.py
+++ angel/angel/report/territory_based_undelivered_sales_order/territory_based_undelivered_sales_order.py
@@ -27,7 +27,7 @@ def get_columns(filters):
 
 def  get_data(filters):
      territory = filters["territory"]
-     frappe.msgprint( _("{0}".format( territory)))
+     #frappe.msgprint( _("{0}".format( territory)))
      data = frappe.db.sql('''SELECT `name`, `territory`, `delivery_status`     
              FROM 
                  `tabSales Order`
