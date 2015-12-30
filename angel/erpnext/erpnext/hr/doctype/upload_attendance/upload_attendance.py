6b40aef4277f24c00d7cfd69b65c190425f0b1b5
shivnamha09 <korecent@erp-server.aeronsolution.com>
2015-12-30 02:29:56 -0500

    added shift displayed for existing records only and set automatically even if its left empty via attendance.py

--- angel/erpnext/erpnext/hr/doctype/upload_attendance/upload_attendance.py
+++ angel/erpnext/erpnext/hr/doctype/upload_attendance/upload_attendance.py
@@ -37,7 +37,7 @@ def add_header(w):
 	w.writerow(["Status should be one of these values: " + status])
 	w.writerow(["If you are overwriting existing attendance records, 'ID' column mandatory"])
 	w.writerow(["ID", "Employee", "Employee Name", "Date", "Status",
-		"Fiscal Year", "Company", "Naming Series", "FingerPrint_ID", "Clock_In", "Clock_Out"])
+		"Fiscal Year", "Company", "Naming Series", "FingerPrint_ID", "Clock_In", "Clock_Out", "Shift"])
 	return w
 
 def add_data(w, args):
@@ -60,6 +60,7 @@ def add_data(w, args):
 				employee.fingerprint_id, #added fingerprint_id
 				existing_attendance and existing_attendance.clock_in or "",
 				existing_attendance and existing_attendance.clock_out or "",
+				employee.shift,
 			]
 			w.writerow(row)
 	return w
@@ -71,7 +72,7 @@ def get_dates(args):
 	return dates
 
 def get_active_employees():
-	employees = frappe.db.sql("""select name, employee_name, company, fingerprint_id
+	employees = frappe.db.sql("""select name, employee_name, company, fingerprint_id, shift
 		from tabEmployee where docstatus < 2 and status = 'Active'""", as_dict=1)
 	return employees
 
