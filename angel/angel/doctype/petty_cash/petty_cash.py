23fd69a9b6ae0c461915f803ead04a904e2cae9e
shivnamha09 <korecent@erp-server.aeronsolution.com>
2016-02-02 07:23:04 -0500

    #12 Resolved Issue of missing fields balance and account_currency

--- angel/angel/doctype/petty_cash/petty_cash.py
+++ angel/angel/doctype/petty_cash/petty_cash.py
@@ -24,3 +24,10 @@ class PettyCash(Document):
                   received_by = self.get("received_by")
                   if  frappe.db.get_value("Petty Cash", {"amount":amount, "posting_date":posting_date, "pay_for":pay_for, "received_by":received_by, "docstatus":1}, "name", as_dict = True):
                          frappe.throw(_("You can't create duplicate doctype"))
+'''
+@frappe.whitelist()
+def get_account_detail(payfor):
+	if payfor:
+		frappe.db.get_value("Account", filters = {"
+	return "Navdeep"
+'''
