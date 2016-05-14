46c361ad83e01a755d105f3ab0e374dd3bad14de
shivnamha09 <korecent@erp-server.aeronsolution.com>
2016-05-14 04:43:23 -0400

    Report for Deal Couner [Navdeep']

--- angel/angel/report/deal_counter_based_sales_orders/deal_counter_based_sales_orders.js
+++ angel/angel/report/deal_counter_based_sales_orders/deal_counter_based_sales_orders.js
@@ -10,6 +10,14 @@ frappe.query_reports["Deal Counter based Sales Orders"] = {
                         "options" : "Deal Counter",
                         "width": "80",
                         "reqd" : 1
-                }
+                },
+		{
+			"fieldname" :"type",
+			"label": __("Type"),
+			"fieldtype": "Select",
+			"options": "Sales Invoice\nSales Order",
+			"default": "Sales Invoice",
+			"width": "100"
+		}
 	]
 }
