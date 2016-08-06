70ad5ce3bc0e0c87c7c6f8decd3a054dac64563b
shivnamha09 <korecent@erp-server.aeronsolution.com>
2016-08-06 08:29:08 -0400

    ..

--- angel/utils.py
+++ angel/utils.py
@@ -1,6 +1,7 @@
 from frappe.utils import flt, cint, cstr
 import frappe
 from frappe import _
+import json
 
 DATE_FORMAT = "%Y-%m-%d"
 EMP_NAME = "EMP/0001"
@@ -9,6 +10,8 @@ END_DATE = "12-01-2016"
 APPLY_ON = "Employee"
 SALES_INVOICE = "SINV-00008"
 COMMISSION_RULE = 5
+
+
 def calculate_sales_commission(outstanding_amount, grand_total, allocated_percentage, commission_rule):
 	if not commission_rule:
 		return 0
@@ -38,3 +41,29 @@ def add_commission(emp_id, from_date , end_date):
 		commission_rule = invoice["commission_rule"]
 		calculated_commission = (calculated_commission + calculate_sales_commission(outstanding_amount, grand_total, allocated_percentage, commission_rule))
 	return calculated_commission
+
+@frappe.whitelist()
+def get_delivery_items(delivery_note= None):
+	items_details = []
+	delivery_note = json.loads(delivery_note)
+	if not delivery_note:
+		return item_details
+	if isinstance(delivery_note, list):
+		items_details = frappe.db.sql("""SELECT item_code, qty, parent,price_list_rate from `tabDelivery Note Item` WHERE 
+					docstatus = 1 AND parent IN (%s) """%" ,".join(['%s']*len(delivery_note)), 
+					tuple(name for name in delivery_note), as_dict =True)
+	temp_list = {}
+	if items_details:
+		for item in items_details:
+			if temp_list.has_key(item['item_code']):
+				temp_list[item['item_code']]['qty'] += item['qty']
+			else:
+				temp_list[item['item_code']] = {}
+				temp_list[item['item_code']]['qty'] = item['qty']
+				temp_list[item['item_code']]['item_name'] = item['item_code']
+	return [items_details, temp_list]
+
+
+
+def item_wise_qty(items_details):
+	pass
