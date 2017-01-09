06a10a7c814f0022fc3488293f8d1348b50389f6
shivnamha09 <chetan.sh009@gmail.com>
2017-01-09 09:52:58 +0100

    angel/angel/doctype/pp_document/pp_document.py

--- angel/angel/doctype/pp_document/pp_document.py
+++ angel/angel/doctype/pp_document/pp_document.py
@@ -7,4 +7,17 @@ import frappe
 from frappe.model.document import Document
 
 class PPDocument(Document):
-	pass
+	
+        def validate(self):
+                pass
+        def on_submit(self):
+                self.update_pp()
+
+        def update_pp(self):
+                tbl = self.outstanding_invoices or []
+                for item in tbl:
+                        voucher_no = item.against_voucher_no
+                        name = self.name
+                        if voucher_no:
+                                frappe.db.sql(""" UPDATE `tabPurchase Invoice` SET pp_reference_number=%(val)s\
+                                                        WHERE name=%(flag)s""" , {"flag":voucher_no, "val":name})
