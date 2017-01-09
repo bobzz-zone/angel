6eff184bef35cc6c438b90ac896241fb99435e37
shivnamha09 <chetan.sh009@gmail.com>
2017-01-09 09:54:21 +0100

    angel/utils.py

--- angel/utils.py
+++ angel/utils.py
@@ -47,23 +47,88 @@ def get_delivery_items(delivery_note= None):
 	items_details = []
 	delivery_note = json.loads(delivery_note)
 	if not delivery_note:
-		return item_details
+		return items_details
 	if isinstance(delivery_note, list):
-		items_details = frappe.db.sql("""SELECT item_code, qty, parent,price_list_rate from `tabDelivery Note Item` WHERE 
-					docstatus = 1 AND parent IN (%s) """%" ,".join(['%s']*len(delivery_note)), 
+		items_details = frappe.db.sql("""SELECT item_code, item_name, qty, description, parent,price_list_rate from `tabDelivery Note Item` 							WHERE docstatus = 1 AND parent IN (%s) """%" ,".join(['%s']*len(delivery_note)), 
 					tuple(name for name in delivery_note), as_dict =True)
 	temp_list = {}
 	if items_details:
 		for item in items_details:
-			if temp_list.has_key(item['item_code']):
-				temp_list[item['item_code']]['qty'] += item['qty']
+			key = (item['parent'], item['item_code'])
+			if temp_list.has_key(key):	
+				temp_list[key]['qty'] += item['qty']
 			else:
-				temp_list[item['item_code']] = {}
-				temp_list[item['item_code']]['qty'] = item['qty']
-				temp_list[item['item_code']]['item_name'] = item['item_code']
-	return [items_details, temp_list]
+				item_detail = {}
+				item_detail[item['item_code']] = item
+				item_detail = map_customer(item['item_code'], item['parent'], item_detail)
+				temp_list[key] = item_detail
+
+	return dict_to_list(temp_list)
 
 
+
+
+def map_customer(item_code, parent, item_detail):
+	customer = frappe.db.get_value("Delivery Note", filters={"name":parent}, fieldname=["customer", "customer_address"], as_dict= True)
+	if customer:
+		item_detail[item_code]['customer'] = customer['customer']
+		item_detail[item_code]['customer_address'] = customer['customer_address']
+	return item_detail		
 
 def item_wise_qty(items_details):
 	pass
+
+
+def dict_to_list(items):
+	data = []
+	if not items:
+		return data
+	for key, val in items.iteritems():
+		data.append(val)
+	return data
+
+
+
+def validate_delivery(l_doc, name):
+	if not (l_doc and name):
+		return l_doc
+	s_doc_list = frappe.get_list("Delivery Note", filters={"is_return":1, "return_against":name, "docstatus":1}, fields="*")
+	if not s_doc_list:
+		return l_doc
+
+	for item in s_doc_list:
+
+		items = frappe.db.sql("""SELECT name, item_code, qty, rate, price_list_rate, amount from `tabDelivery Note Item` WHERE \
+					parent = '%s'"""%(item.get("name")), as_dict=True)
+		l_doc = update_parent(l_doc, item)
+		if not items:
+			continue
+	
+		for item in items:
+			l_doc = update_item(item, l_doc)
+
+	l_doc  = post_delivery_validation(l_doc)
+	return l_doc
+
+
+
+def update_item(item, doc):
+	for idx in range(0,len(doc.items)):
+		if doc.items[idx].item_code == item.item_code:
+			doc.items[idx].qty += item.qty
+			doc.items[idx].amount += item.amount
+	return doc	
+
+def update_parent(l_doc, s_doc):
+	l_doc.grand_total += s_doc.get("grand_total")
+	l_doc.total += s_doc.get("total")
+	l_doc.rounded_total += s_doc.get("rounded_total")
+	l_doc.total_taxes_and_charges = s_doc.get("total_taxes_and_charges")
+	return l_doc
+
+def post_delivery_validation(l_doc):
+	for idx in range(0, len(l_doc.items)):
+		item = l_doc.items[idx]
+		if item.qty == 0:
+			del l_doc.items[idx]
+	return l_doc
