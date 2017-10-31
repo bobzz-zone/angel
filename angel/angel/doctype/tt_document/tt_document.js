67b5047d400d17d0cfe4a614a679723b8b247db2
Bobby <bobzz.zone@gmail.com>
2017-10-31 16:44:53 +0000

    aa

---
+++ angel/angel/doctype/tt_document/tt_document.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Korecent Solutions Pvt Ltd and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('TT Document', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ angel/angel/doctype/tt_document/tt_document_list.js
@@ -0,0 +1,21 @@
+frappe.listview_settings['TT Document'] = {
+	add_fields: ["status", "total_amount", "sales_person"],
+	// added by chetan by checking from listivew.js
+	//add_columns: ["status", "total_amount", "sales_person"],
+	selectable: true,
+	get_indicator: function(doc) {
+		if(doc.status=="Due for Payment") {
+			return[__("Due for Payment"), "orange", "status,=,Due for Payment"];
+		}
+		else if((flt(doc.total_amount == 0))) {
+			return[__("Settled"), "green", "status,=,Settled"];
+		}
+	},
+	onload:function(doc){
+//	//	console.log(doc);
+//		 $(".primary-action").hide();
+		 $(".btn-danger").show();	 
+	}
+	
+
+};
