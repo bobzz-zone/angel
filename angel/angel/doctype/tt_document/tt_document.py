b2582d1b713ec8298a6c81df397a5d8a3c271659
Bobby <bobzz.zone@gmail.com>
2018-01-05 09:28:51 +0000

    aa

--- angel/angel/doctype/tt_document/tt_document.py
+++ angel/angel/doctype/tt_document/tt_document.py
@@ -9,21 +9,23 @@ from frappe import _, msgprint, throw
 
 class TTDocument(Document):
 
-	'''
+	
 	def onload(self):
 		lst = []
 		grand_total = 0
 		tbl = self.get("outstanding_invoices")
                 dest = []
+		om=0
 		for arr in tbl:
 			against_v = arr.against_voucher_no
 			val = frappe.db.get_value("Sales Invoice",{"name":against_v},"outstanding_amount")
-                        grand_total += arr.total_amount
+			grand_total += arr.total_amount
 			arr.outstanding_amount = val
-                        dest.append(arr)
+			dest.append(arr)
+			om+=val
 	        self.set("outstanding_invoices", dest)
 		self.set("grand_total", grand_total)
-	'''		
+		self.set("total_amount",om)
 	def validate(self):
 		pass
 	def on_submit(self):
